837774a90cb46ef49bbd3a014bdf23aec6e9be0e
Salvatore Alaimo <alaimos@gmail.com>
2023-04-09 20:14:07 +0200

    fixes

--- src/client/pages/_document.tsx
+++ src/client/pages/_document.tsx
@@ -1,19 +1,20 @@
 // noinspection HtmlRequiredTitleElement
 
-import {Head, Html, Main, NextScript} from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
-const DESCRIPTION = "tRFUniverse is a public database through which users may explore tRNA-derived ncRNAs";
+const DESCRIPTION =
+    "tRFUniverse is a public web app through which users may analyze tRNA-derived ncRNAs in TCGA, TARGET, NCI-60, and biological fluids.";
 
 export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <meta name="description" content={DESCRIPTION}/>
-                <link rel="icon" href="/favicon.ico"/>
+                <meta name="description" content={DESCRIPTION} />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
             <body className="g-sidenav-show bg-gray-100">
-            <Main/>
-            <NextScript/>
+                <Main />
+                <NextScript />
             </body>
         </Html>
     );
